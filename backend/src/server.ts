c46a8f323f3e856b5565da73102094d2cb96ca0e
sigehiro <sigehiro.0728@gmail.com>
2025-01-23 03:45:31 -0500

    一時的connct()をコメントアウト、server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,20 +8,20 @@ const app = express()
 app.use(express.json())
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI!, {})
-        console.log('MongoDB Connected')
-    } catch (err) {
-        console.error(err)
-        process.exit(1)
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI!, {})
+    console.log('MongoDB Connected')
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
+  }
 }
 
-connectDB()
+// connectDB()
 
 //初期での起動確認：curl -i http://localhost:3000/
 app.get('/', (req, res) => {
-    res.send('Hello World')
+  res.send('Hello World')
 })
 
 const PORT = process.env.PORT || 3000
